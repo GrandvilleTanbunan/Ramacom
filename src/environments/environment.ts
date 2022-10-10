58708e966faed14d75b7072b53838e0bf5515158
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2022-10-10 21:01:59 +0800

    tambah firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ramacom-6b0d1',
+    appId: '1:314425500488:web:3ea921c69f5fa5eb39c97d',
+    storageBucket: 'ramacom-6b0d1.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBgmDHoMhXxqyP7nn7TPRcK3PwNUph2D1c',
+    authDomain: 'ramacom-6b0d1.firebaseapp.com',
+    messagingSenderId: '314425500488',
+    measurementId: 'G-Z5MF63PVS3',
+  },
   production: false
 };
 
