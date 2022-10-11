3b6170a36e9c2995cfd124b63ba4372988376211
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2022-10-11 17:16:03 +0800

    Berhasil cegah balik ke halaman login

--- src/app/stock-admin/stock-admin.page.ts
+++ src/app/stock-admin/stock-admin.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform, IonRouterOutlet, AlertController } from '@ionic/angular';
+import { App } from '@capacitor/app';
+
 
 @Component({
   selector: 'app-stock-admin',
@@ -7,7 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockAdminPage implements OnInit {
 
-  constructor() { }
+  constructor(public platform: Platform, private routerOutlet: IonRouterOutlet, public alertCtrl: AlertController) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        this.presentConfirm();
+        // App.exitApp();
+        
+      }
+    });
+   }
+   async presentConfirm() {
+    let alert = await this.alertCtrl.create({
+      
+      message: 'Anda yakin ingin keluar aplikasi?',
+      buttons: [
+        {
+          text: 'Tidak',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'YA',
+          handler: () => {
+            App.exitApp();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
 
   ngOnInit() {
   }
--- src/app/stock-cabang/stock-cabang.page.ts
+++ src/app/stock-cabang/stock-cabang.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Platform, IonRouterOutlet, AlertController } from '@ionic/angular';
+import { App } from '@capacitor/app';
 
 @Component({
   selector: 'app-stock-cabang',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockCabangPage implements OnInit {
 
-  constructor() { }
+  constructor(public platform: Platform, private routerOutlet: IonRouterOutlet, public alertCtrl: AlertController) {
+    this.platform.backButton.subscribeWithPriority(-1, () => {
+      if (!this.routerOutlet.canGoBack()) {
+        this.presentConfirm();
+        // App.exitApp();
+        
+      }
+    });
+   }
 
+   
   ngOnInit() {
   }
+
+   async presentConfirm() {
+    let alert = await this.alertCtrl.create({
+      
+      message: 'Anda yakin ingin keluar aplikasi?',
+      buttons: [
+        {
+          text: 'Tidak',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'YA',
+          handler: () => {
+            App.exitApp();
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
 
 }
