0382acc0e96c0acaf9f8164ce6585cb72e046d47
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2022-10-11 09:20:47 +0800

    baru buat tampilan untuk side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Stock', url: '/stock-admin', icon: '/assets/images/stock.png' },
+    { title: 'Penjualan', url: '/penjualan-admin', icon: '/assets/images/selling.png' },
+    { title: 'Pembelian', url: '/pembelian-admin', icon:'/assets/images/buy.png' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+
+  public setting = [
+    { title: 'Pengaturan', url: '/home/Stock', icon: '/assets/images/setting.png' },
+    { title: 'Logout', url: '/home/Stock', icon: '/assets/images/logout.png' },
+  ]
+  
+  // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
