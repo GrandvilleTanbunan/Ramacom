3eb495118851c9ce05bf5750110eec4a43f6b297
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2022-10-11 10:20:17 +0800

    Revert "berhasil pakai service"
    This reverts commit b787262701cb2ae9109d5e6d69c0d6bde7f43496.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { AuthService } from './auth.service';
 import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
@@ -6,56 +5,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  username = '';
-  public appPages = [];
-  public setting = [];
+  public appPages = [
+    { title: 'Stock', url: '/stock-admin', icon: '/assets/images/stock.png' },
+    { title: 'Penjualan', url: '/penjualan-admin', icon: '/assets/images/selling.png' },
+    { title: 'Pembelian', url: '/pembelian-admin', icon:'/assets/images/buy.png' },
+  ];
 
+  public setting = [
+    { title: 'Pengaturan', url: '/home/Stock', icon: '/assets/images/setting.png' },
+    { title: 'Logout', url: '/home/Stock', icon: '/assets/images/logout.png' },
+  ]
+  
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor(public authService: AuthService) {
-    this.authService.loginStatus$.subscribe((isLoggedIn) => {
-      if (isLoggedIn == 'admin') {
-        console.log('ini adalah admin')
-
-      }
-      else {
-        console.log('ini BUKAN admin')
-
-      }
-
-    })
-  
-
-  
-  // ionViewWillEnter(){
-  //   this.authService.loginStatus$.subscribe((isLoggedIn) => {
-  //     if (isLoggedIn == 'admin') {
-  //       console.log('ini adalah admin')
-
-  //     }
-  //     else {
-  //       console.log('ini BUKAN admin')
-
-  //     }
-
-  //   })
-  
-    // if(this.authService.isAdmin())
-    // {
-    //   console.log('ini adalah admin')
-    //   this.appPages = [
-    //     { title: 'Stock', url: '/stock-admin', icon: '/assets/images/stock.png' },
-    //     { title: 'Penjualan', url: '/penjualan-admin', icon: '/assets/images/selling.png' },
-    //     { title: 'Pembelian', url: '/pembelian-admin', icon:'/assets/images/buy.png' },
-    //   ];
-
-    //   this.setting = [
-    //     { title: 'Pengaturan', url: '/home/Stock', icon: '/assets/images/setting.png' },
-    //     { title: 'Logout', url: '/home/Stock', icon: '/assets/images/logout.png' },
-    //   ]
-    // }
-    // else
-    // {
-    //   console.log('ini bukan admin')
-    // }
-  }
+  constructor() {}
 }
