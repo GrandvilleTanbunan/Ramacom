c9df4b3716ad1d17bc4018956e6ea4768d3a0c47
GrandvilleTanbunan <46751850+GrandvilleTanbunan@users.noreply.github.com>
2022-12-09 09:50:25 +0800

    batas sebelum mulai auth

--- src/app/penjualan-admin/penjualan-admin.page.ts
+++ src/app/penjualan-admin/penjualan-admin.page.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 // import { format, parseISO } from 'date-fns';
 import * as moment from 'moment';
+import { Platform, IonRouterOutlet, AlertController, ToastController, ModalController, LoadingController   } from '@ionic/angular';
+// import { ModalExampleComponent } from './modal-example.component';
+import { AddTypeModalComponent } from '../add-type-modal/add-type-modal.component';
+import { App } from '@capacitor/app';
+import { DataService } from './../services/data.service';
+import { collection } from '@firebase/firestore';
+import { collectionData, docData, Firestore, doc, addDoc } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import {of} from 'rxjs'
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-penjualan-admin',
@@ -11,7 +21,9 @@ export class PenjualanAdminPage implements OnInit {
 
   public tmpselectedDate = new Date().toISOString();
   public selectedDate
-  constructor() { }
+  constructor(private loadingCtrl: LoadingController, private db: AngularFirestore, private modalCtrl: ModalController, private firestore: Firestore, private toastCtrl: ToastController, private dataService: DataService, public platform: Platform, private routerOutlet: IonRouterOutlet, public alertCtrl: AlertController) {
+    
+   }
 
   ngOnInit() {
   }
@@ -20,6 +32,12 @@ export class PenjualanAdminPage implements OnInit {
     this.selectedDate = moment(this.tmpselectedDate).format('DD/MM/YYYY')
     console.log(this.selectedDate);
     
+  }
+
+  Dismissmodal()
+  {
+    this.modalCtrl.dismiss();
+
   }
 
 }
